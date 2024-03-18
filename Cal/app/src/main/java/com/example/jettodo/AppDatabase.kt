cc157b756e2c125bd393336f661cb94e122954f6
Ko Tsukayama <ko202020tsuka@gmail.com>
2024-03-18 21:27:54 +0900

    2nd commit

--- Cal/app/src/main/java/com/example/jettodo/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jettodo
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [CalList::class], version = 1, exportSchema = false)
-abstract class AppDatabaseCalList : RoomDatabase() {
-    abstract  fun callistDao(): CalListDao
-}
