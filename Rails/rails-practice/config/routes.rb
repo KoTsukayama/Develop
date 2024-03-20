351a926dd1f8f83042b8415df25766ff29204f1a
Ko Tsukayama <ko202020tsuka@gmail.com>
2024-03-20 21:03:07 +0900

    Boards create form

--- Rails/rails-practice/config/routes.rb
+++ Rails/rails-practice/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "boards#index"
+  get 'boards', to: 'boards#index'
+  get 'boards/new', to: 'boards#new'
 end
 
