d4874aa44a64d8cbe946fdca76b84c38812e342d
Ko Tsukayama <ko202020tsuka@gmail.com>
2024-04-01 21:09:14 +0900

    Create Form

--- Java/its 3/src/main/java/com/exam/its/web/issue/IssueController.java
+++ Java/its 3/src/main/java/com/exam/its/web/issue/IssueController.java
@@ -6,20 +6,36 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/issues")
 public class IssueController {
 
     private final IssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model){
 
         model.addAttribute("issueList", issueService.findAll());
         return "issues/list";
+    }
+
+    // GET /issues/creationForm
+    @GetMapping("/creationForm")
+    public String showCreationForm(){
+        return "issues/creationForm";
+    }
+
+    // POST /isssue
+    @PostMapping
+    public String create(IssueForm form, Model model){
+        // TODO　データの永続化
+        return showList(model); // TODO リロードボタン対策が必要
     }
 }
 
---
+++ Java/its 3/src/main/java/com/exam/its/web/issue/IssueForm.java
@@ -0,0 +1,9 @@
+package com.exam.its.web.issue;
+
+import lombok.Data;
+
+@Data
+public class IssueForm {
+    private String summary;
+    private String description;
+}
