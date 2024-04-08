58fb207ba945b3e40372b406f3b1c995516777a6
Ko Tsukayama <ko202020tsuka@gmail.com>
2024-04-09 06:48:11 +0900

    Create Navbar

--- React/library-app/03-frontend/react-library/src/App.tsx
+++ React/library-app/03-frontend/react-library/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NabbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to the fullstack cource</h1>
-    </div>
+    <Navbar/>
   );
 }
 
