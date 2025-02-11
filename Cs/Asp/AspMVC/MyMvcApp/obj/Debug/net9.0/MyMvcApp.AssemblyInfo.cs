a4c099ea030b4e00b203eeddde9f778cf88cd0b9
Ko Tsukayama <ko202020tsuka@gmail.com>
2025-02-11 10:39:00 +0900

    Add Comment User

--- Cs/Asp/AspMVC/MyMvcApp/obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
+++ Cs/Asp/AspMVC/MyMvcApp/obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0320a48bed20ae9777d48da8bf7aee2d4ee501b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bfd4ef9364fc7c5c63a41b49c5cff8f886df828")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
