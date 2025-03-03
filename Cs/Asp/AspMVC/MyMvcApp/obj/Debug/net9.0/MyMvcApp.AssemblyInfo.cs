ce983a6a72fd15a77b982e997103b20a45e958fe
Ko Tsukayama <ko202020tsuka@gmail.com>
2025-03-04 06:17:04 +0900

    Modify clearing removeall

--- Cs/Asp/AspMVC/MyMvcApp/obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
+++ Cs/Asp/AspMVC/MyMvcApp/obj/Debug/net9.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4539e05eaac3664f075ff0db3c81bce7eb5929db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5425580349d267a71fb40ad77d6e3376089a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
